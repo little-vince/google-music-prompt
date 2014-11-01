a9c48f5ff38d65ab7353c0532cd96d48219db385
little-vince <littlevince09@gmail.com>
2014-11-01 16:28:51 +1100

    Added download/update urls for Greasemonkey.

--- google.music.prompt.user.js
+++ google.music.prompt.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name           	Google Play Music Close Prompt
-// @description    	If music is playing, it will prompt you before closing the tab
-// @version        	0.1
-// @grant          	none
-// @author         	little-vince
-// @namespace      	http://little-vince.tumblr.com/
-// @include        	*play.google.com/music/listen*
+// @name            Google Play Music Close Prompt
+// @description     If music is playing, it will prompt you before closing the tab
+// @version         0.1
+// @grant           none
+// @author          little-vince
+// @namespace       http://little-vince.tumblr.com/
+// @include         *play.google.com/music/listen*
+// @updateURL       https://raw.githubusercontent.com/little-vince/google-music-prompt/master/google.music.prompt.user.js
+// @downloadURL     https://raw.githubusercontent.com/little-vince/google-music-prompt/master/google.music.prompt.user.js
 // ==/UserScript==
 
 // Last edited 25 February 2014
